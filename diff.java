d2e42a224a189072027750af832f19babe73ac1f
dighitay <dighitayerunkar@gmail.com>
2026-04-13 23:29:36 +0530

    adding diff.java

---
+++ diff.java
@@ -0,0 +1,12 @@
+public class Diff { 
+ 
+    public static void main(String[] args){ 
+ 
+        int a = 10; 
+        int b = 4; 
+ 
+        System.out.println("Difference is " + (a - b)); 
+ 
+    } 
+ 
+} 
